b25760af82a74a818b112dfec06920a91ba2986b
Dillon Uzar <dillonu@gmail.com>
2015-05-12 15:28:30 -0400

    Update Marble Defaults

--- FinalProject/GenerateMarble.h
+++ FinalProject/GenerateMarble.h
@@ -5,10 +5,10 @@
 #include "Image.h"
 #include "Simplex.h"
 
-Image* GenerateMarble(int width, int height, double xFactor = 5.0 / 1000.0, double yFactor = 12.0 / 1000.0, double turbFactor = 1.0 / 300.0, double turbPower = 3.0, int turbSize = 10) {
+Image* GenerateMarble(int width, int height, double xFactor = 5.0 / 1000.0, double yFactor = 12.0 / 1000.0, double turbFactor = 1.0 / 800.0, double turbPower = 3.0, int turbSize = 10) {
 	// Initialize Variables:
 	Image *terrain = new Image(width, height); // Output Map
-	double noise, amplitude, invZoom = 1.0 / 75.0; // Calculation Variables
+	double noise, amplitude; // Calculation Variables
 	int value;
 	const double PI = 3.141592653589793238463;
 
@@ -22,7 +22,7 @@ Image* GenerateMarble(int width, int height, double xFactor = 5.0 / 1000.0, doub
 			double xyValue = x*xFactor + y*yFactor + turbPower*generator.Turbulence(x*turbFactor, y*turbFactor, turbSize);
 			noise = fabs(sin(xyValue*PI));
 
-			// Convert the -1.0 to 1.0 noise value to a 0 to 255 RGB value:
+			// Convert the 0.0 to 1.0 noise value to a 0 to 255 RGB value:
 			value = int(noise * 256);
 			if (value > 255) value = 255;
 			if (value < 0) value = 0;
